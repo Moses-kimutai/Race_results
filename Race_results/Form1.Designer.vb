565a67deee86915fda70175918e61e056485e2c4
jamal <jamal@JAMALMUDI>
2021-10-21 12:03:23 -0400

    Add project files.

---
+++ Race_results/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Lblthird = New System.Windows.Forms.Label()
+        Me.Lblsecond = New System.Windows.Forms.Label()
+        Me.Lblfirst = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(252, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(340, 64)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the three runner's names and finishing times."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(307, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 23)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(570, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(218, 23)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Finishing time(in seconds)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(67, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 23)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Runner 1:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(67, 204)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 23)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Runner 2:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(67, 260)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 23)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Runner 3:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox1.Controls.Add(Me.Lblthird)
+        Me.GroupBox1.Controls.Add(Me.Lblsecond)
+        Me.GroupBox1.Controls.Add(Me.Lblfirst)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(67, 307)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(637, 264)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Race Results."
+        '
+        'Lblthird
+        '
+        Me.Lblthird.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Lblthird.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Lblthird.Location = New System.Drawing.Point(345, 172)
+        Me.Lblthird.Name = "Lblthird"
+        Me.Lblthird.Size = New System.Drawing.Size(209, 25)
+        Me.Lblthird.TabIndex = 5
+        '
+        'Lblsecond
+        '
+        Me.Lblsecond.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Lblsecond.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Lblsecond.Location = New System.Drawing.Point(345, 107)
+        Me.Lblsecond.Name = "Lblsecond"
+        Me.Lblsecond.Size = New System.Drawing.Size(209, 25)
+        Me.Lblsecond.TabIndex = 4
+        '
+        'Lblfirst
+        '
+        Me.Lblfirst.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Lblfirst.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Lblfirst.Location = New System.Drawing.Point(345, 44)
+        Me.Lblfirst.Name = "Lblfirst"
+        Me.Lblfirst.Size = New System.Drawing.Size(209, 25)
+        Me.Lblfirst.TabIndex = 3
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(77, 178)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(77, 20)
+        Me.Label9.TabIndex = 2
+        Me.Label9.Text = "3rd place:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(77, 113)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 20)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "2nd place:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(77, 50)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(75, 20)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "1st place:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(298, 120)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(298, 189)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(298, 256)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(598, 120)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox4.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(598, 189)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox5.TabIndex = 11
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(598, 256)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox6.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(83, 619)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 64)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Calculate results"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(298, 619)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 64)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "CLEAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(555, 619)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 64)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "EXIT"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 695)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Lblthird As Label
+    Friend WithEvents Lblsecond As Label
+    Friend WithEvents Lblfirst As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Race_results/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaration of variables'
+        Dim FirstName As String
+        Dim SecondName As String
+        Dim ThirdName As String
+        Dim FirstRunnerTime As Double
+        Dim SecondRunnerTime As Double
+        Dim ThirdRunnerTime As Double
+        'tranfer of textboxex inputs to RunnerTimes'
+        Try
+            FirstRunnerTime = CDbl(TextBox4.Text)
+            SecondRunnerTime = CDbl(TextBox5.Text)
+            ThirdRunnerTime = CDbl(TextBox6.Text)
+        Catch
+            MessageBox.Show("Enter a double number!")
+
+        End Try
+        'if statement to detemine diferent conditions. use of AND to mean addition. both condition should br true for the answer to be true.'
+        If (FirstRunnerTime < SecondRunnerTime) And (SecondRunnerTime < ThirdRunnerTime) Then
+            Lblfirst.Text = TextBox1.Text
+            Lblsecond.Text = TextBox2.Text
+            Lblthird.Text = TextBox3.Text
+        ElseIf (FirstRunnerTime < ThirdRunnerTime) And (ThirdRunnerTime < SecondRunnerTime) Then
+            Lblfirst.Text = TextBox1.Text
+            Lblsecond.Text = TextBox3.Text
+            Lblthird.Text = TextBox2.Text
+        ElseIf (SecondRunnerTime < FirstRunnerTime) And (FirstRunnerTime < ThirdRunnerTime) Then
+            Lblfirst.Text = TextBox2.Text
+            Lblsecond.Text = TextBox1.Text
+            Lblthird.Text = TextBox3.Text
+        ElseIf (SecondRunnerTime < ThirdRunnerTime) And (ThirdRunnerTime < FirstRunnerTime) Then
+            Lblfirst.Text = TextBox2.Text
+            Lblsecond.Text = TextBox3.Text
+            Lblthird.Text = TextBox1.Text
+        ElseIf (ThirdRunnerTime < FirstRunnerTime) And (FirstRunnerTime < SecondRunnerTime) Then
+            Lblfirst.Text = TextBox3.Text
+            Lblsecond.Text = TextBox1.Text
+            Lblthird.Text = TextBox2.Text
+        ElseIf (ThirdRunnerTime < SecondRunnerTime) And (SecondRunnerTime < FirstRunnerTime) Then
+            Lblfirst.Text = TextBox3.Text
+            Lblsecond.Text = TextBox2.Text
+            Lblthird.Text = TextBox1.Text
+        Else
+            Lblfirst.Text = ("invalid")
+            Lblsecond.Text = ("invalid")
+            Lblthird.Text = ("invalid")
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' clearance of the textboxes and display labels for new calculation'
+        TextBox1.ResetText()
+        TextBox2.ResetText()
+        TextBox3.ResetText()
+        TextBox4.ResetText()
+        TextBox5.ResetText()
+        TextBox6.ResetText()
+        Lblfirst.ResetText()
+        Lblsecond.ResetText()
+        Lblthird.ResetText()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'exit the application'
+        Me.Close()
+
+    End Sub
+End Class
